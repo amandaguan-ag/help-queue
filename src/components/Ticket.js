8b17799774710e72ab2d6171d161938470668643
Amanda Guan <ag.amandaguan@gmail.com>
2023-08-05 23:17:01 -0700

    Show Ticket Detail

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -4,13 +4,16 @@ import PropTypes from "prop-types";
 function Ticket(props) {
   return (
     <React.Fragment>
-      <h3>
-        {props.location} - {props.names}
-      </h3>
-      <p>
-        <em>{props.issue}</em>
-      </p>
-      <hr />
+      <div onClick={() => props.whenTicketClicked(props.id)}>
+        {/* We add a div with an onClick function. Don't forget to close out the div below! */}
+        <h3>
+          {props.location} - {props.names}
+        </h3>
+        <p>
+          <em>{props.issue}</em>
+        </p>
+        <hr />
+      </div>
     </React.Fragment>
   );
 }
@@ -18,10 +21,9 @@ function Ticket(props) {
 Ticket.propTypes = {
   names: PropTypes.string,
   location: PropTypes.string,
-//We can also make a property required by adding isRequired:
-  // names: PropTypes.string.isRequired,
-  // location: PropTypes.string.isRequired,
   issue: PropTypes.string,
+  id: PropTypes.string, // new PropType
+  whenTicketClicked: PropTypes.func, // new PropType
 };
 
 export default Ticket;
--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -1,6 +1,7 @@
 import React from "react";
 import NewTicketForm from "./NewTicketForm";
 import TicketList from "./TicketList";
+import TicketDetail from "./TicketDetail";
 
 class TicketControl extends React.Component {
   constructor(props) {
@@ -8,17 +9,24 @@ class TicketControl extends React.Component {
     this.state = {
       formVisibleOnPage: false,
       mainTicketList: [],
-      selectedTicket: null,
+      selectedTicket: null, // new code
     };
     // Bind the handleClick method
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick() {
-    this.setState((prevState) => ({
-      formVisibleOnPage: !prevState.formVisibleOnPage,
-    }));
-  }
+  handleClick = () => {
+    if (this.state.selectedTicket != null) {
+      this.setState({
+        formVisibleOnPage: false,
+        selectedTicket: null,
+      });
+    } else {
+      this.setState((prevState) => ({
+        formVisibleOnPage: !prevState.formVisibleOnPage,
+      }));
+    }
+  };
 
   handleAddingNewTicketToList = (newTicket) => {
     const newMainTicketList = this.state.mainTicketList.concat(newTicket);
@@ -28,18 +36,37 @@ class TicketControl extends React.Component {
     });
   };
 
+  handleChangingSelectedTicket = (id) => {
+    const selectedTicket = this.state.mainTicketList.filter(
+      (ticket) => ticket.id === id
+    )[0];
+    this.setState({ selectedTicket: selectedTicket });
+  };
+
   render() {
     let currentlyVisibleState = null;
     let buttonText = null;
-    if (this.state.formVisibleOnPage) {
+
+    if (this.state.selectedTicket != null) {
+      currentlyVisibleState = (
+        <TicketDetail ticket={this.state.selectedTicket} />
+      );
+      buttonText = "Return to Ticket List";
+      // While our TicketDetail component only takes placeholder data, we will eventually be passing the value of selectedTicket as a prop.
+    } else if (this.state.formVisibleOnPage) {
+      // This conditional needs to be updated to "else if."
       currentlyVisibleState = (
         <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList} />
-      ); // Pass the method as a prop to NewTicketForm
+      );
       buttonText = "Return to Ticket List";
     } else {
       currentlyVisibleState = (
-        <TicketList ticketList={this.state.mainTicketList} />
+        <TicketList
+          ticketList={this.state.mainTicketList}
+          onTicketSelection={this.handleChangingSelectedTicket}
+        />
       );
+      // Because a user will actually be clicking on the ticket in the Ticket component, we will need to pass our new handleChangingSelectedTicket method as a prop.
       buttonText = "Add Ticket";
     }
     return (
