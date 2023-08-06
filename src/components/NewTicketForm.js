cb878607afcaaf9b03bb787e366869010a2052a5
Amanda Guan <ag.amandaguan@gmail.com>
2023-08-05 23:21:09 -0700

    add ReusableForm

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,27 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types"; //import PropTypes
 import { v4 } from "uuid"; // new code
+import ReusableForm from "./ReusableForm";
+
 
   function handleNewTicketFormSubmission(event) {
     event.preventDefault();
     props.onNewTicketCreation({
-      names: event.target.names.value, 
-      location: event.target.location.value, 
-      issue: event.target.issue.value, 
-      id: v4()
+      names: event.target.names.value,
+      location: event.target.location.value,
+      issue: event.target.issue.value,
+      id: v4(),
     });
 
-  return (
-    <React.Fragment>
-      <form onSubmit={handleNewTicketFormSubmission}>
-        <input type="text" name="names" placeholder="Pair Names" />
-        <input type="text" name="location" placeholder="Location" />
-        <textarea name="issue" placeholder="Describe your issue." />
-        <button type="submit">Help!</button>
-      </form>
-    </React.Fragment>
-  );
-}
+    return (
+      <React.Fragment>
+        <ReusableForm
+          formSubmissionHandler={handleNewTicketFormSubmission}
+          buttonText="Help!"
+        />
+      </React.Fragment>
+    );
+  }
 
 // We also need to add PropTypes for our new prop.
 
